faff19845e6c54b440627f46e4b0a3a0505418ce
divyanshu98163 <divy9anshu>
2024-07-17 20:16:14 +0530

    Array list

---
+++ .arrraylist/A1.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+public class A1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.err.println(list);
+
+        // get element 
+        int element = list.get(2);
+        System.err.println(element);
+
+        // Remove
+        int element2 =list.remove(2);
+
+        System.err.println(list);
+
+        // set element 
+        list.set(3, 8);
+        System.err.println(list);
+
+        // contain element
+
+       
+        System.err.println(list.contains(1));
+        
+        System.err.println(list.contains(22));
+    }
+}
