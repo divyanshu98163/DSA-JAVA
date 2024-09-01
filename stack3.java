d58ec62bc957a8e8fb66cd2f9caae192ac50d268
divyanshu98163 <divy9anshu>
2024-09-01 11:54:12 +0530

    Q3 Revrse a stack

---
+++ stack3.java
@@ -0,0 +1,45 @@
+
+import java.util.*;
+
+public class stack3 {
+
+    public static void pushBottem(Stack<Integer> s ,int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushBottem(s, data);
+        s.push(top);
+
+    }
+    public static void reversestack(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top = s.pop();
+        reversestack(s);
+        pushBottem(s, top);
+
+    }
+    public static void printStack(Stack<Integer> s){
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+            
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        reversestack(s);
+        printStack(s);
+
+        
+
+    }
+}
