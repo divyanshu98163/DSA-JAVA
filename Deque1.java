bd15be4b629918b110a646cfe3ac26bef85204f6
divyanshu98163 <divy9anshu>
2024-09-05 21:45:30 +0530

    Deque using java collection fream work

---
+++ Deque1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.LinkedList;
+public class Deque1 {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addLast(3);
+        deque.addLast(4);
+        System.out.println(deque);
+
+        deque.removeFirst();
+        deque.removeLast();
+
+        System.out.println(deque);
+        System.err.println("get first "+deque.getFirst());
+        System.err.println("get last "+ deque.getLast());
+
+    }
+    
+}
