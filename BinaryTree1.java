b0a04690a1176f2da60a8a0babb713e9d14176ee
divyanshu98163 <divy9anshu>
2024-09-11 12:04:11 +0530

    Height sum diameter

--- BinaryTree1.java
+++ BinaryTree1.java
@@ -9,6 +9,9 @@ public class BinaryTree1 {
             this.right = null;
         }
     }
+
+    // HEIGHT 
+
     public static int Height(Node root){
         if(root == null){
             return 0;     
@@ -17,6 +20,9 @@ public class BinaryTree1 {
         int rh = Height(root.right);
         return Math.max(lh, rh)+1;
     }
+
+    // count 
+
     public static int count(Node root){
         if(root==null){
             return 0;
@@ -25,13 +31,52 @@ public class BinaryTree1 {
         int rightCount = count(root.right);
         return leftCount + rightCount+1;
     }
+
+    // sum 
+
     public static int sum(Node root){
         if(root==null){
             return 0;
         }
-        int leftCount = sum(root.left);
-        int rightCount = sum(root.right);
-        return leftCount + rightCount+1;
+        int leftSum = sum(root.left);
+        int rightSum = sum(root.right);
+        return leftSum + rightSum+root.data;
+    }
+    // diameter -> approcah 1st  tc = O(n)
+
+    public static int diameter(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftDim = diameter(root.left);
+        int rightDim = diameter(root.right);
+        int lh = Height(root.left);
+        int rh = Height(root.right);
+
+        int selfDim = lh+rh+1;
+        return Math.max(selfDim, Math.max(leftDim, rightDim));
+    }
+
+    // diameter ->approach 2 /// O(n)
+    static class Info{
+        int diam;
+        int ht;
+        public Info(int diam,int ht){
+            this.diam =diam;
+            this.ht= ht;
+        }
+    }
+    public static Info diameter2(Node root){ 
+        if(root == null){
+            return new Info(0, 0);
+        }
+
+        Info leftInfo = diameter2(root.left);
+        Info rightInfo = diameter2(root.right);
+        int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam),leftInfo.ht+rightInfo.ht+1); 
+        int ht = Math.max(leftInfo.ht,rightInfo.ht)+1;
+
+        return  new Info(diam, ht);
     }
 
     public static void main(String[] args) {
@@ -49,5 +94,7 @@ public class BinaryTree1 {
         System.out.println("count "+count(root));
 
         System.out.println("sum "+sum(root));
+
+        System.err.println("dimeter "+diameter2(root).diam);
     }
 }
